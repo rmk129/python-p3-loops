4d5e85552a66160eacedf03a938ac847887a84dd
Romil <romil.m.kothari@gmail.com>
2023-09-20 14:56:08 -0400

    tests completed

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,32 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i >= 0:
+        if i == 0:
+            print("Happy New Year!")
+            i -= 1
+        else:
+            print(f"{i}")
+            i -= 1
+
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [integer * integer for integer in int_list]
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    i = 1
+    while i <= 100:
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+            i += 1
+        elif i % 3 == 0:
+            print("Fizz")
+            i += 1
+        elif i % 5 == 0:
+            print("Buzz")
+            i +=1
+        else:
+            print(f'{i}')
+            i += 1
